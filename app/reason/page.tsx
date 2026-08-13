433fa55eb23e166104a188bc5b2532485f799c01
Varun Kumar <varun27040>
2026-08-14 05:09:59 +0530

    Fix reason page TypeScript build

---
+++ app/reason/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ReasonPage() {
+  return (
+    <main>
+      <h1>Our Story 💖</h1>
+      <p>There are so many reasons why this journey is special.</p>
+    </main>
+  );
+}
